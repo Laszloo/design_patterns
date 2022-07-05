8ca67472194ccb945dc26d2b4d39fb1fd4bef04e
Elek László <e.laszlo96@gmail.com>
2022-07-05 11:49:06 +0200

    add ob function to MVC

--- src/MVC/ViewRenderer.php
+++ src/MVC/ViewRenderer.php
@@ -7,8 +7,9 @@ namespace App\MVC;
 class ViewRenderer
 {
     public function render(View $view){
-
+        ob_clean();
         extract($view->getData());
         require __DIR__.DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR.$view->getName().".phtml";
+        ob_end_flush();
     }
 }
